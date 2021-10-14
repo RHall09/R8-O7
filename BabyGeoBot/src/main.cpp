70734373094baba57726f3a51d3eeab37904b5dd
Kai <91277460+PirateKai@users.noreply.github.com>
2021-10-13 20:22:23 -0700

    updates to geobaby bot to include Adafruit MotorDriver featherwing class. Current issues with PWMServoDriver inclusion and compiling error.

--- BabyGeoBot/src/main.cpp
+++ BabyGeoBot/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(3);
 
 void setup() {
   // put your setup code here, to run once:
+  AFMS.begin();
+  leftMotor->setSpeed(100);
+  rightMotor->setSpeed(100); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  delay (5000);
 }
