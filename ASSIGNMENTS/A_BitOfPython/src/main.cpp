3209b8c65da5c94f934906062185f3357d5ac058
Rick Hall <rhall09@calpoly.edu>
2021-10-24 21:21:18 -0700

    Why does VScode do this stuff

--- ASSIGNMENTS/A_BitOfPython/src/main.cpp
+++ ASSIGNMENTS/A_BitOfPython/src/main.cpp
@@ -5,12 +5,22 @@
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.begin (115200);
+  delay (2000);
+
+  Serial << endl << "Hello, I'm in ME507" << " ...and I vote." << endl;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+Serial << "A random array of booleans is:     ";
 
+float why[7] = { 8.777, -15.321, 0, 255, 12, 88, 147} ;
 
+print_array(why, 7) ;  
 
+delay(1000);
+
+Serial << "..." << endl;
 
 }
