a5dde077b8b825e125fee66dd0aff57f00506f87
Rick Hall <rhall09@calpoly.edu>
2021-10-31 15:15:42 -0700

    Organization of Repo, Word doc w/ Program Design, etc.

---
+++ Drivers/SD_Driver_Project/src/a-SD_driver.h
@@ -0,0 +1,41 @@
+/** @file a_SD_driver.h
+ *    This file contains a class designed to operate the Adafruit 
+ *    MicroSD Card Brreakout Board+, with specific commands intended
+ *    for the operation of a geofence for the R8-O7 project.
+ *
+ *  @author Rick Hall
+ *  @date 2020-10-Oct 
+ */
+
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+
+/**
+ * 
+ * 
+ * 
+ */
+class MicroSD 
+{
+
+protected:
+
+
+
+
+
+
+public:
+
+//Constructor Function
+MicroSD (void);
+
+//
+
+
+
+
+
+}
