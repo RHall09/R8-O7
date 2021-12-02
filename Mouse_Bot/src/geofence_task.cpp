5ff66899a4762738fc9654a171c8ec6b5d98a333
Kai <91277460+PirateKai@users.noreply.github.com>
2021-12-01 23:31:26 -0800

    Initial push of Mouse_Bot. The updated Baby bot Running in a geofence

---
+++ Mouse_Bot/src/geofence_task.cpp
@@ -0,0 +1,43 @@
+/** @file geofence_task.cpp
+ *  This file contains a task which calculates the shortest straight line distance
+ *  from the bots current position to the edge of the geofence
+ * 
+ *  @author Kai Quizon
+ *  @date   2021-Nov-27 Original file 
+ */
+
+#include <Arduino.h>
+#if (defined STM32L4xx || defined STM32F4xx)
+    #include <STM32FreeRTOS.h>
+#endif
+#include "geofence_task.h"
+#include "shares.h"
+#include "Geofence_Drivers/geofencing.h"
+
+#define EVER (;;)
+
+void geofence_task (void* p_params) {
+    float lat;                    // Holds lattitude of bot from sensor suite task
+    float longitude;              // Holds longitude of bot from sensor suite task
+    float fence_lat[] = {0,0,0};  // The geofence lattitude points to be used
+    float fence_long[] = {0,0,0}; // The geofence longitude points to be used
+    uint8_t fence_size = 3;       // The number of points defining the geofence
+    float heading;                // The heading returned by the geofence task locating hte nearest geofence point
+    float distance;               // The distance returned by the geofence task indicating the closest geofence point
+
+    for EVER {
+        // Grab the updated GPS data from the sensor suite task, if available.
+        // This line is blocking, meaning that this task only runs when the GPS data
+        // is updated. For now this means we aren't using a delay, however it may be needed
+        // depending on how often GPS data is updated.
+        lat       = latitude_queue.get();
+        longitude = longitude_queue.get();
+
+        distance, heading = geofencing(lat, longitude, fence_lat, fence_long, fence_size);
+        fence_distance.put(distance);
+        fence_heading.put(heading);
+        // vTaskDelay(5)    Currently delays for 5 seconds
+
+    }
+
+}
---
+++ Mouse_Bot/src/shares.h
@@ -0,0 +1,28 @@
+/** @file shares.h
+ *  This file contains extern declarations of shares and queues which are used
+ *  in more than one file of a demonstration project.
+ * 
+ *  @author JR Ridgely
+ *  @date   2021-Oct-23 Original file
+ *  @copyright (c) 2021 by JR Ridgely, released under the LGPL 3.0. 
+ */
+ 
+#ifndef _SHARES_H_
+#define _SHARES_H_
+
+#include <ME507_Support/taskqueue.h>
+#include <ME507_Support/taskshare.h>
+#include <Misc_Drivers/RegOperators.h>
+
+// A queue which stores the latitude from the GPS module
+extern Queue<float> latitude_queue;
+// A queue which stores the longitude from the GPS module
+extern Queue<float> longitude_queue;
+//
+extern Queue<float> fence_distance;
+//
+extern Queue<float> fence_heading;
+
+
+
+#endif // _SHARES_H_
