01e8bc03684c6c13a0e817801857bf5901ba2fd3
Rick Hall <rhall09@calpoly.edu>
2021-12-05 16:45:45 -0800

    Nav task mostly done, still need to do documentation for new nav class

--- R8-O7_MAIN/src/navigation_task.cpp
+++ R8-O7_MAIN/src/navigation_task.cpp
@@ -18,26 +18,53 @@
 
 #include <PrintStream.h>
 #include "shares.h"
+#include "cmath"
+#include <Navigation_Drivers/nav_module.h>
 
-#define FENCE_MIN 3
+// Minimum distance to fence
+#define FENCE_MIN (float)3
+// Calibration for conversion ft/s to pwm
+#define V_CAL (float)0.3
+// Calibration for conversion deg/s to pwm
+#define T_CAL (float)0.25
+// Preferred robot velocity
+#define CRUISE_VEL (float) 3
 
 #define EVER (;;)
 
 void navigation_task(void* p_param) {
     // Geofence Heading variable
-    float fence_head;
+    float fnc_head;
     // Geofence Distance variable
-    float fence_dist;
-    // GPS Heading
-    // float gps_head = 0;
+    float fnc_dist;
+    // GPS Forward Heading
+    // float gps_fore = 0;
 
     // Mathematical Heading
-    float current_heading = 0;
+    float current_heading = 0;      // Turn rate is in deg/s
+    // Mathematical Velocity
+    float current_velocity = 0;     // Velocity is in ft/s
+
+    // Desired Turnrate
+    float set_turn = 0;     // Turn rate is in deg/s
+    // Desired Velocity
+    float set_vel = 0;      // Velocity is in ft/s
+
+    // Motor Set A
+    int16_t set_A = 0;      // PWM from -255 to +255
+    // Motor Set B
+    int16_t set_B = 0;      // PWM from -255 to +255
+
+    // Navigation decremental timer to determine point turns
+    uint16_t nav_timer = 0;
 
     // Local copy of shared motor flag register
     uint8_t current_flag = 0;
     current_flag = motorFlagRegister.get();
     current_flag = bit_set(Run_Motor_CS);
+
+    // Navigation calculation object
+    Navigation nav(V_CAL,T_CAL);
 
     Serial << "Nav task is setup" << endl;
 
@@ -48,19 +75,58 @@ void navigation_task(void* p_param) {
         
         if(!bit_chk(Overcurrent_A) | !bit_chk(Overcurrent_B))
         {
-            // First update the distance to the fence and the heading
-            fence_head = fence_heading.get();
-            fence_dist = fence_distance.get();
+            // So long as the nav delay timer is not set, run algorithm
+            if(nav_timer = 0)
+            {
+                // First update the distance to the fence and the heading
+                fnc_head = fence_heading.get();
+                fnc_dist = fence_distance.get();
 
-            if (0 < (fence_dist - FENCE_MIN)) 
-            {
-                motorSet_A_q.put(0);
-                motorSet_B_q.put(100);
+                // If the fence distance is greater than the minimum by at least 0.5 ft
+                // and the geofence heading is outside of a 30d area infront of the bot
+                if ( (0.5 < (fnc_dist - FENCE_MIN)) && (((float)90 - fnc_head) > (float)15) )
+                {
+                    // Run natural fence avoidance algorithm
+
+                    static float r = 0;
+
+                    set_vel = CRUISE_VEL;
+
+                    r = (fnc_dist - FENCE_MIN)/(1 - cos((90 - fnc_head) * (PI / (float)180)));
+
+                    set_turn = (set_vel / r) * ((float)180 / PI);
+
+                }
+                else if (fnc_dist < (float)10) // otherwise
+                {
+                    // Stop and turn away from the fence
+
+                    set_vel = 0;
+                    set_turn = 5;
+
+                    // Set timer to turn until robot is parallel to the fence plus a random angle between 30 and 150 degrees
+                    nav_timer = (uint16_t)round(abs(((((float)90 - fence_heading) + (rand() % 120 + 30))/set_turn)));
+
+                }
+
+                // Convert speed/turn_rate to motor_A/motor_B
+                nav.nav_conv(set_vel, set_turn);
+                set_A = nav.nav_A();
+                set_B = nav.nav_B();
+
+                // Put computed PWM values in queues for Motor task
+                motorSet_A_q.put(set_A);
+                motorSet_B_q.put(set_B);
             }
-            else 
+            else
             {
-                motorSet_A_q.put(150);
-                motorSet_B_q.put(150);
+                nav_timer = nav_timer - 1;
+
+                // Software timer underflow protection
+                if (nav_timer < 0)
+                {
+                    nav_timer = 0;
+                }
             }
         }
         
--- R8-O7_MAIN/src/navigation_task.h
+++ R8-O7_MAIN/src/navigation_task.h
@@ -1,10 +1,12 @@
 /** @file navigation_task.h
- *  This file contains a task that determines the path of the
- *  robot and directs the motors accordingly.
+ *  This file contains a task which takes inputs from all sensors 
+ *  and the GPS to determine the path of the robot and output motor
+ *  PWM values accordingly.
  * 
  *  @author Rick Hall
  *  @date   2021-Dec-02 Original file 
  */
+
 #ifndef _TASK_NAVIGATION_
 #define _TASK_NAVIGATION_
 void navigation_task (void* p_params);
