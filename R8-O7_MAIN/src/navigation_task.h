7c66993658ff22977879aa0e0244c5f327fd86cb
Kai <91277460+PirateKai@users.noreply.github.com>
2021-12-06 15:56:29 -0800

    Updates to task function doxystrings. Deleted old, now defunct STM suite task

--- R8-O7_MAIN/src/navigation_task.h
+++ R8-O7_MAIN/src/navigation_task.h
@@ -1,13 +1,23 @@
-/** @file navigation_task.h
- *  This file contains a task which takes inputs from all sensors 
+/** @headerfile navigation_task.h ""
+ *  This file acts as a header for a task which takes inputs from all sensors 
  *  and the GPS to determine the path of the robot and output motor
  *  PWM values accordingly.
  * 
  *  @author Rick Hall
  *  @date   2021-Dec-02 Original file 
+ *  @copyright (c) 2021 by Rick Hall, released under the LGPL 3.0.
  */
 
 #ifndef _TASK_NAVIGATION_
 #define _TASK_NAVIGATION_
+/**
+ *  @brief   This task runs the navigation of the bot within its geofence.
+ *  @details This task updatest the motor setpoints by synthesizing data from
+ *           the safety sensor suite and the geofence distances. It also determines
+ *           if it is a safe instance for ShowSense moment. If a safe moment is available,
+ *           trigger the ShowSense moment. Ultimately, this task asks as a general "brain"
+ *           for R8-07.
+ *  @param   p_params A pointer to function parameters which we don't use.
+ */
 void navigation_task (void* p_params);
 #endif // _TASK_NAVIGATION_
